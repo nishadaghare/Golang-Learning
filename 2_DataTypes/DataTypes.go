f75ce1fc791dc650ea8d739042d554c0e879c75d
Nishada Ghare <66076780+nishadaghare@users.noreply.github.com>
2025-10-28 17:58:12 +0530

    DataTypes

---
+++ 2_DataTypes/DataTypes.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b float32
+	var c string
+	var d bool
+	var e byte
+	var f rune
+	a = 1
+	b = 5.3
+	c = "Apple"
+	d = true
+	e = 'N'
+	f = 'd'
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+
+}
